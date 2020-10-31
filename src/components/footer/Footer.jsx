648f797f814c36c4d9a81fac2f43ea26186e8fa1
Subash Yadav <ysubash03@gmail.com>
2020-10-31 15:35:45 +0545

    ready for production

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = ({ onInputChange }) => {
   return (
     <Fragment>
     <div className="footer">
-    <p><b>Created by <a href="https://ysubash.com.np/" target="_blank">@mathachew7</a></b></p>
+    <p><b>Created by <a href="https://ysubash.com.np/" target="_blank" rel="noreferrer">@mathachew7</a></b></p>
     <p>No Copyright. Star me On Github</p>
   </div>
     </Fragment>
